eda79a9c03465d1004ca7fe2375935e7eb0273d8
Gino Gustavo <ing.gino@gmail.com>
2019-09-13 06:31:33 -0500

    Change first message of the class

--- bigo/src/main/java/edu/training/App.java
+++ bigo/src/main/java/edu/training/App.java
@@ -1,13 +1,10 @@
 package edu.training;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World2!" );
+        System.out.println( "Practice with Big oh notation" );
     }
 }
