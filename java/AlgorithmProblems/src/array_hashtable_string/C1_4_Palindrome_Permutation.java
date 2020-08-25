7050be32371edd25b540dae0bba3ec7395a452b4
Gino Gustavo <ing.gino@gmail.com>
2020-08-25 11:14:38 -0500

    Palidrome Permutation method 1

---
+++ java/AlgorithmProblems/src/array_hashtable_string/C1_4_Palindrome_Permutation.java
@@ -0,0 +1,36 @@
+package array_hashtable_string;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class C1_4_Palindrome_Permutation {
+
+	public static void main(String[] args) {
+
+		String text = "tact coa";
+		System.out.println(new C1_4_Palindrome_Permutation().validPalindrome(text));
+
+	}
+
+	// Method 1: check duplicates (author)
+
+	private boolean validPalindrome(String text) {
+
+		Map<Character, Integer> duplicates = new Hashtable<Character, Integer>();
+
+		for (int i = 0; i < text.length(); i++) {
+
+			Character letter = text.charAt(i);
+			if (letter != ' ') { // ignore non letters characters
+				if (duplicates.containsKey(letter)) {
+					duplicates.remove(letter);
+				} else {
+					duplicates.put(letter, 1);
+				}
+			}
+		}
+		System.out.println(duplicates.size());
+		return duplicates.size() <= 1;
+
+	}
+}
